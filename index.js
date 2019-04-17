470b9e0fb942fac97a8ce3e0b44efd68558131e7
Aliya Lewis <aliyalewis5@gmail.com>
2019-04-17 15:06:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 var katzDeliLine = []
 
-function takeANumber(currentLine, newPerson) {
-  currentLine.push(newPerson)
-  return(`Welcome, ${newPerson}. You are number ${currentLine.length} in line.`);
+function takeANumber(line, name) {
+line.push(name);
+return `Welcome, ${name}. You are number ${line.length} in line.`
 }
 
-function nowServing(katzDeliLine){
-  if (katzDeliLine.length > 0){
+function nowServing(katzDeliLine) {
+  if (katzDeliLine.length > 0) {
     return `Currently serving ${katzDeliLine.shift()}.`
   } else {
     return "There is nobody waiting to be served!"
@@ -14,17 +14,17 @@ function nowServing(katzDeliLine){
 }
 
 function currentLine(line) {
-  if (line.length > 0) {
+  if (line.length > 0 ) {
     var numbersAndName = "The line is currently: "
-    for(let i=0; i < line.length; i++){
-      if (line.length-1 === i) {
+  for (let i=0; i<line.length; i++) {
+    if (line.length-1 === i) {
       numbersAndName += `${i+1}. ${line[i]}`
-      } else {
-        numbersAndName += `${i+1}. ${line[i]}, `
-      }
+    } else {
+      numbersAndName += `${i+1}. ${line[i]}, `
     }
-    return numbersAndName
-  } else {
+  }
+  return numbersAndName
+} else {
     return "The line is currently empty."
   }
 }
